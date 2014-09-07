25b47d773d300dbea2b140a202ffbaf6a4454757
2540548 <s9rathei@stud.uni-saarland.de>
2014-09-07 21:23:19 +0200

    Map generator 3.0

--- utility/MapGenerator/src/Main.java
+++ utility/MapGenerator/src/Main.java
@@ -1,49 +1,60 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Random;
+
+import javax.imageio.ImageIO;
 
 public class Main {
 
-	private static final char emptyIsland = '#';
-	private static final char water = '.';
-	private static final char treasure = '$';
-	
-	private static final double treasureChance = 0.3;
-	private static final double baseChance = 0.7;
-	private static final double islandChance = 1;
-
 	private static Integer height;
 	private static Integer width;
-	private static Integer islandsCount;
-	private static Integer minIslandSize = 80;
-	private static Integer maxIslandSize = 300;
 	private static Integer teamsPerMap;
-	private static Integer boatsPerTeam;
-	private static int[] bases;
-	private static int[] islandSizes;
-	private static char[][] fields;
-	
-	private static boolean baseMustBeNearIsland = false;		//broken
-	
+	private static Integer islandSize;
 	
 	public static void main(String[] args) throws FileNotFoundException{
-				
-		height = Integer.parseInt(args[0]);
-		width  = Integer.parseInt(args[1]);
-		islandsCount = Integer.parseInt(args[2]);
-		teamsPerMap  = Integer.parseInt(args[3]);
-		boatsPerTeam = Integer.parseInt(args[4]);
-		
-		islandSizes = new int[islandsCount];
-		bases = new int[teamsPerMap];
-		fields = new char[height][width];
-		
-		build();
-		write();
+
+		if(args[0].equals("generate")){
+			
+			//Standard configuration: 'generate 200 200 40 5 5 8'
+			height = Integer.parseInt(args[1]);
+			width  = Integer.parseInt(args[2]);
+			Integer islandsCount = Integer.parseInt(args[3]);
+			islandSize = Integer.parseInt(args[4]);
+			teamsPerMap  = Integer.parseInt(args[5]);
+			Integer boatsPerTeam = Integer.parseInt(args[6]);
+			
+			if(!checkInput()) return;
+			
+			Generator gen = new Generator(height, width, islandsCount, islandSize, teamsPerMap, boatsPerTeam);
+			write(gen.generate());	
+		}
+		else if(args[0].equals("transform")){
+			
+			//Standard configuration: 'transform europe.jpg 5 8'
+			String mapFile = args[1];
+			teamsPerMap  = Integer.parseInt(args[2]);
+			Integer boatsPerTeam = Integer.parseInt(args[3]);
+			
+			Color[][] image = read(mapFile);
+			if(image == null) return;
+			
+			Transformer trans = new Transformer(image);
+			char[][] fields = trans.transform();
+			height = fields.length;
+			width = fields[0].length;
+			Generator gen = new Generator(fields, teamsPerMap, boatsPerTeam);
+
+			write(gen.placeObjects());
+		}
+		else
+			System.out.println("args[0] must be either 'generate' to generate a random map or 'transform' to create a map from an image file(args[1])");
 	}
 	
 	
-	private static void write() throws FileNotFoundException{
+	private static void write(char[][] fields) throws FileNotFoundException{
 		
 		PrintWriter out = new PrintWriter("map.txt");
 		out.write(height.toString() + '\n');
@@ -62,212 +73,54 @@ public class Main {
 		out.close();
 	}
 	
-	
-	private static void build(){	
+	public static Color[][] read(String fileName){
 		
-		for(int y = 0; y < height; y++){
-			for(int x = 0; x < width; x++)
-				fields[y][x] = water;
+		BufferedImage image;
+		File file = new File(fileName);
+		
+		try{
+			image = ImageIO.read(file);
 		}
-		setIslands();
-		setTreasures();
-		setBases();
-		cleanUp();
+		catch(IOException e){
+			System.out.println("No Image found");
+			return null;
+		}
+		
+		if(image.getHeight() < 2 || image.getHeight() > 1999){
+			System.out.println("Height must be in range {2...2000}");
+			return null;
+		}
+		if(image.getWidth() < 2 || image.getWidth() > 1999){
+			System.out.println("Width must be in range {2...2000}");
+			return null;
+		}
+		
+		Color[][] img = new Color[image.getHeight()][image.getWidth()];
+		for(int y = 0; y < image.getHeight(); y++){
+			for(int x = 0; x < image.getWidth(); x++)
+				img[y][x] = new Color(image.getRGB(x, y));
+		}	
+		return img;
 	}
 	
-	private static void setIslands(){
-		Random gen = new Random();
-		
-		for(int i = 0; i < islandsCount; i++){
-			islandSizes[i] = minIslandSize + gen.nextInt(maxIslandSize - minIslandSize);
-			
-			boolean set = false;
-			while(!set){
-				int x = gen.nextInt(width);
-				int y = gen.nextInt(height);
-				
-				if(fields[y][x] == water){
-					fields[y][x] = emptyIsland;
-					islandSizes[i]--;
-					generateIslandSwarm(i, y, x);
-					set = true;
-				}
-			}
-		}
-	}
-	
-
-	private static void setTreasures(){
-		Random gen = new Random();
-		
-		for(int y = 0; y < height; y++){
-			for(int x = 0; x < width; x++){
-				
-				float pick = gen.nextFloat();
-				if((fields[y][x] == emptyIsland) && (pick < treasureChance)){
-					if(hasNeighbor(y, x, water))
-						fields[y][x] = treasure;
-				}	
-			}
-		}
-	}
-	
-	private static void setBases(){
-		Random gen = new Random();
-		
-		for(int i = 0; i < bases.length; i++){
-			boolean set = false;
-			while(!set){
-				int x = gen.nextInt(width);
-				int y = gen.nextInt(height);
-				
-				if(fields[y][x] == water){
-					if(baseMustBeNearIsland && (hasNeigbor(y, x, emptyIsland) < 3))
-						continue;
-					
-					fields[y][x] = teamName(i);
-					generateBaseSwarm(i, y, x);
-					set = true;
-				}
-			}
-		}
-	}
-	
-	
-	private static void generateBaseSwarm(int teamNumber, int y, int x){
-		Random gen = new Random();
-		int[] ys = {y-1, y, y+1};
-		int[] xs = {x-1, x, x+1};
-		
-		while(countBases(teamNumber) < boatsPerTeam){
-			int xPick;
-			if(y%2 == 0)
-				xPick = gen.nextInt(xs.length-1);
-			else
-				xPick = 1 + gen.nextInt(xs.length-1);
-		
-			int yPick = gen.nextInt(ys.length);
-			
-			if(generateBase(teamNumber, ys[yPick], xs[xPick]))
-				generateBaseSwarm(teamNumber, ys[yPick], xs[xPick]);	
-		}
-	}
-	
-	private static boolean generateBase(int teamNumber, int y, int x){
-		if(countBases(teamNumber) < boatsPerTeam){
-			
-			Random gen = new Random();
-			float pick = gen.nextFloat();
-			if(pick < baseChance){
-				fields[mod(y, height)][mod(x, width)] = teamName(teamNumber);
-				return true;
-			}			
-		}
+	private static boolean checkInput(){
+		if(height < 2 || height > 1999){
+			System.out.println("Height must be in range {2...2000}");
 		return false;
-	}
-	
-	private static void generateIslandSwarm(int islandIndex, int y, int x){
-		Random gen = new Random();
-		int[] ys = {y-1, y, y+1};
-		int[] xs = {x-1, x, x+1};
-		
-		while(islandSizes[islandIndex] > 0){
-			int xPick;
-			if(y%2 == 0)
-				xPick = gen.nextInt(xs.length-1);
-			else
-				xPick = 1 + gen.nextInt(xs.length-1);
-		
-			int yPick = gen.nextInt(ys.length);
-			
-			if(generateIsland(islandIndex, ys[yPick], xs[xPick]))
-				generateIslandSwarm(islandIndex, ys[yPick], xs[xPick]);	
 		}
-	}
-	
-	private static boolean generateIsland(int islandIndex, int y, int x){
-		if(islandSizes[islandIndex] > 0){
-			
-			Random gen = new Random();
-			float pick = gen.nextFloat();
-			if(pick < islandChance){
-				fields[mod(y, height)][mod(x, width)] = emptyIsland;
-				islandSizes[islandIndex]--;
-				return true;
-			}			
+		if(width < 2 || width > 1999){
+			System.out.println("Width must be in range {2...2000}");
+			return false;
 		}
-		return false;
-	}
-	
-	private static int countBases(int teamNumber){
-		
-		int counter = 0;
-		for(int y = 0; y < height; y++){
-			for(int x = 0; x < width; x++){
-				if(fields[y][x] == teamName(teamNumber))
-					counter++;
-			}
+		if(islandSize < 1 || islandSize > 10){
+			System.out.println("The island size must be in range {1...10} \n => 1='very large' ... 10='very small'");
+			return false;
 		}
-		return counter;
-	}
-	
-	private static int hasNeigbor(int y, int x, char fieldType){
-		int counter = 0;
-		int xx = mod(x-1, width);
-		int yy = mod(y-1, height);
-		
-		if(fields[yy][xx] == fieldType)
-			counter++;
-		xx = mod(x, width);
-		if(fields[yy][xx] == fieldType)
-			counter++;
-		xx = mod(x + 1, width);
-		if(fields[yy][xx] == fieldType)
-			counter++;
-		yy = mod(y, width);
-		if(fields[yy][xx] == fieldType)
-			counter++;
-		xx = mod(x - 1, width);
-		if(fields[yy][xx] == fieldType)
-			counter++;
-		yy = mod(y - 1, width);
-		if(fields[yy][xx] == fieldType)
-			counter++;
-		xx = mod(x, width);
-		if(fields[yy][xx] == fieldType)
-			counter++;
-		xx = mod(x, width);
-		if(fields[yy][xx] == fieldType)
-			counter++;
-		
-		return counter;
-	}
-	
-	private static boolean hasNeighbor(int y, int x, char fieldType){
-		if(hasNeigbor(y, x, fieldType) > 0)
-			return true;
-		
-		return false;
-	}
-	
-	private static void cleanUp(){
-		for(int y = 0; y < height; y++){
-			for(int x = 0; x < width; x++){
-			
-				if(fields[y][x] == water){
-					int neighborCount = hasNeigbor(y, x, emptyIsland) + hasNeigbor(y, x, treasure); 
-					if(neighborCount > 6)
-						fields[y][x] = emptyIsland;
-				}
-			}
+		if(teamsPerMap < 2 || teamsPerMap > 25){
+			System.out.println("Team count must be in range {2...16}");
+			return false;
 		}
+		return true;
 	}
 	
-	private static int mod(int a, int b){
-		return (a%b + b) % b;
-	}
-	
-	private static char teamName(int number){
-		return ((char) (97 + number));
-	}
 }
---
+++ utility/MapGenerator/src/Transformer.java
@@ -0,0 +1,51 @@
+import java.awt.Color;
+
+
+public class Transformer {
+
+	private final char emptyIsland = '#';
+	private final char water = '.';
+	
+	private int height;
+	private int width;
+	
+	private Color[][] image;
+	private char[][] fields;
+	
+	
+	public Transformer(Color[][] img){
+		
+		image = img;
+		height = image.length;
+		width = image[0].length;
+		fields = new char[height][width];
+	}
+	
+	
+	public char[][] transform(){
+		
+		for(int y = 0; y < height; y++){
+			for(int x = 0; x < width; x++){
+				
+				int r = image[y][x].getRed();
+				int g = image[y][x].getGreen();
+				int b = image[y][x].getBlue();
+				int black = 120;
+				
+				if((r < b) && (g < b))
+					fields[y][x] = water;
+				else if((r < black) && (g < black) && (b < black))
+					fields[y][x] = water;
+				else if(b < 50)
+					fields[y][x] = water;
+				else
+					fields[y][x] = emptyIsland;
+				/*
+				if(b < 180)
+					fields[y][x] = emptyIsland;
+				*/
+			}
+		}	
+		return fields;
+	}
+}
