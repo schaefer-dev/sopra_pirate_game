d552e106f88bf6a769dcf7e250c4d5de4fd00f9c
Daniel Schäfer <schaefer.dev@gmail.com>
2014-09-11 14:43:47 +0200

    TestObjectConstructor Change

--- simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/TestObjectConstructor.java
+++ simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/TestObjectConstructor.java
@@ -1,5 +1,9 @@
 package de.unisaarland.cs.st.pirates.group5.tests;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 import model.Base;
 import model.Buoy;
 import model.Island;
@@ -14,16 +18,37 @@ import model.Water;
 
 public class TestObjectConstructor {
 	
-	public Base[] testBases; 
-	public Buoy[] testBouys;
-	public Island[] testIslands;
-	public Kraken[] testKraken;
-	public Map[] testMaps; 
-	public ProvisionIsland[] testProvisionIsland;
-	public Ship[] testShips; 
-	public Team[] testTeams;
-	public Treasure[] testTreasures;
-	public Water[] testWater;
+	public List<Base> testBases; 
+	public List<Buoy> testBouys;
+	public List<Island> testIslands;
+	public List<Kraken> testKraken;
+	public List<Map> testMaps; 
+	public List<ProvisionIsland> testProvisionIsland;
+	public List<Ship> testShips; 
+	public List<Team> testTeams;
+	public List<Treasure> testTreasures;
+	public List<Water> testWater;
+	
+	
+	public TestObjectConstructor(int seed) {
+		Random randomGen = new Random(seed);
+		this.testBases = new LinkedList();
+		this.testBouys =new LinkedList();
+		this.testIslands = new LinkedList();
+		this.testKraken = new LinkedList();
+		this.testMaps = new LinkedList();
+		this.testProvisionIsland = new LinkedList();
+		this.testShips = new LinkedList();
+		this.testTeams = new LinkedList();
+		this.testTreasures = new LinkedList();
+		this.testWater = new LinkedList();	
+	}
+	
+	private void generate(){
+		
+		
+	}
+
 	
 	
 }
