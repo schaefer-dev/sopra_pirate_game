414e1cbd51e839a5cddefcd1118c5376f27df283
Daniel Schäfer <schaefer.dev@gmail.com>
2014-09-11 14:32:07 +0200

    added attributes to TestObjectConstructor

--- simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/TestObjectConstructor.java
+++ simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/TestObjectConstructor.java
@@ -1,6 +1,29 @@
 package de.unisaarland.cs.st.pirates.group5.tests;
+
+import model.Base;
+import model.Buoy;
+import model.Island;
+import model.Kraken;
+import model.Map;
+import model.ProvisionIsland;
+import model.Ship;
+import model.Team;
+import model.Treasure;
+import model.Water;
  
 
 public class TestObjectConstructor {
+	
+	public Base[] testBases; 
+	public Buoy[] testBouys;
+	public Island[] testIslands;
+	public Kraken[] testKraken;
+	public Map[] testMaps; 
+	public ProvisionIsland[] testProvisionIsland;
+	public Ship[] testShips; 
+	public Team[] testTeams;
+	public Treasure[] testTreasures;
+	public Water[] testWater;
+	
 	
 }
