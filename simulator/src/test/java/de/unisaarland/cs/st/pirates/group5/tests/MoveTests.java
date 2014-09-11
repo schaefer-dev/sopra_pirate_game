61b18bad882ee199400be5df69e9f356dbe6e7fb
Janna Herrmann <s9jnherr@stud.uni-saarland.de>
2014-09-11 15:40:08 +0200

    movetests01

---
+++ simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/MoveTests.java
@@ -0,0 +1,50 @@
+package de.unisaarland.cs.st.pirates.group5.tests;
+import org.junit.Test;
+
+public class MoveTests {
+
+	@Test
+	 public void testMoveWater(){
+		 
+	 }
+	
+	@Test
+	public void testMoveShipFriend(){
+		
+	}
+	
+	@Test
+	public void testMoveShipEnemyWin(){
+		
+	}
+	
+	@Test
+	public void testMoveShipEnemyLoose(){
+		
+	}
+	
+	@Test
+	public void testMoveKraken(){
+		
+	}
+	
+	@Test
+	public void testMoveBaseEnemy(){
+		
+	}
+	
+	@Test
+	public void testMoveBaseFriend(){
+		
+	}
+	
+	@Test
+	public void testMoveIsland(){
+		
+	}
+	
+	@Test
+	public void testMoveProvisionIsland(){
+		
+	}
+}
