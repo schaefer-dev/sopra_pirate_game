bc5c671aa512d4ba388c3fae3c21cfe91b61a549
Andreas <s9anmey2@stud.uni-saarland.de>
2014-09-12 10:35:56 +0200

    aussagekraeftigeVariablenNamenErgaenzt

--- simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/MapTests.java
+++ simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/MapTests.java
@@ -30,12 +30,12 @@ public class MapTests {
 	Kraken kraken = new Kraken(1, null);
 	List<Kraken> krakens = new ArrayList<Kraken>();
 	Water water = new Water(map, 0, 0, kraken);
-	Island island = new Island(map, 1,0);
-	Water water2 = new Water(map,0,1,null);
-	Water water3 = new Water(map,3,1,null);
-	Water water4 = new Water(map,3,0,null);
-	Island island2 = new Island(map,0,3);
-	Island island3 = new Island(map,3,3);
+	Island geradeaus = new Island(map, 1,0);
+	Water linksUnten = new Water(map,0,1,null);
+	Water rechtsUnten = new Water(map,3,1,null);
+	Water hinten = new Water(map,3,0,null);
+	Island rechtsOben = new Island(map,0,3);
+	Island linksOben = new Island(map,3,3);
 	
 	
 	
@@ -45,12 +45,12 @@ public class MapTests {
 		krakens.add(kraken);
 		
 		fields[0][0] = water;
-		fields[1][0] = island;
-		fields[0][1] = water2;
-		fields[3][1] = water3;
-		fields[3][0] = water4;
-		fields[0][3] = island2;
-		fields[3][3] = island3;
+		fields[1][0] = geradeaus;
+		fields[0][1] = linksUnten;
+		fields[3][1] = rechtsUnten;
+		fields[3][0] = hinten;
+		fields[0][3] = rechtsOben;
+		fields[3][3] = linksOben;
 		
 		map.setMapValues(fields, 4, 2, first, krakens);
 	}
@@ -58,12 +58,12 @@ public class MapTests {
 	@Test
 	public void test(){
 		
-		assertEquals("",map.getNeighbour(water, 0), island);
-		assertEquals("",map.getNeighbour(water, 1), water2);
-		assertEquals("",map.getNeighbour(water, 2), water3);
-		assertEquals("",map.getNeighbour(water, 3), water4);
-		assertEquals("",map.getNeighbour(water, 4), island2);
-		assertEquals("",map.getNeighbour(water, 5), island3);
+		assertEquals("",map.getNeighbour(water, 0), geradeaus);
+		assertEquals("",map.getNeighbour(water, 1), linksUnten);
+		assertEquals("",map.getNeighbour(water, 2), rechtsUnten);
+		assertEquals("",map.getNeighbour(water, 3), hinten);
+		assertEquals("",map.getNeighbour(water, 4), rechtsOben);
+		assertEquals("",map.getNeighbour(water, 5), linksOben);
 
 		assertEquals("",map.getFirstShip(), first);
 			map.setFirstShip(second);
