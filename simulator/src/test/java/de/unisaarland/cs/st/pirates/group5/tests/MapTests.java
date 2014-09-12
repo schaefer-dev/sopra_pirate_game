3f39b47fca6bfe103a5e5a0933839b3842c7ad18
Andreas <s9anmey2@stud.uni-saarland.de>
2014-09-12 09:52:18 +0200

    map

--- simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/MapTests.java
+++ simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/MapTests.java
@@ -2,6 +2,9 @@ package de.unisaarland.cs.st.pirates.group5.tests;
 
 
 import model.Map;
+import model.Ship;
+import model.Field;
+import model.Kraken;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,4 +12,20 @@ import org.junit.Test;
 public class MapTests {
 
 	private Map map;
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@Test
+	public void test(){
+		map.getFirstShip();
+		map.getKraken();
+		map.getLogWriter();
+		map.getNeighbour(field, direction);
+		map.giveNewActorID();
+		map.giveNewEntityID();
+		map.setFirstShip(ship);
+		map.setMapValues(fields, nextFreeActorId, nextFreeEntityId, firstShip, kraken);
+	}
 }
