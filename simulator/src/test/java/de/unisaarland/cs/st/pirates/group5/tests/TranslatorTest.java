1d5754f926234b10b23f170bb8e963c9e15f9289
Jan <s9jamenz@stud.uni-saarland.de>
2014-09-12 10:12:40 +0200

    new Test in TranslatorTest.java

--- simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/TranslatorTest.java
+++ simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/TranslatorTest.java
@@ -1,4 +1,5 @@
 package de.unisaarland.cs.st.pirates.group5.tests;
+import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.LinkedList;
@@ -13,7 +14,9 @@ import commands.Move;
 import commands.Pickup;
 import commands.Sense;
 import commands.Turn;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 import controller.*;
 import model.CellType;
@@ -40,10 +43,29 @@ public class TranslatorTest {
 	@Test
 	public void testExampleTactic(){
 		String tactic = lines.get(lines.size()-1);
-		byte[] temp = tactic.getBytes();
-		InputStream in = new ByteArrayInputStream(temp);
+		InputStream in = stringToStream(tactic);
 		List<Command> erg = translator.run(in);
 		
-		assertEquals("Translaten der Beispieltaktik liefert falsche Commandliste",commands,erg);
+		assertEquals("translation of the example tactic did not result in the correct list of commands",commands,erg);
+	}
+	
+	@Test
+	public void testNoSpacesInComparisons(){
+		String badTactic = "move else 1\n"
+							+"if ship_load == 3 else 0";
+		InputStream in = stringToStream(badTactic);
+		try{
+		translator.run(in);
+		fail("Translator should not allow spaces within comparisons");
+		}
+		catch(IllegalArgumentException e){}
+	}
+	
+	private InputStream stringToStream(String tactic)
+	{
+		byte[] temp = tactic.getBytes();
+		InputStream in = new ByteArrayInputStream(temp);
+		in = new BufferedInputStream(in);
+		return in;
 	}
 }
