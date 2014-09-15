e1b886e374ab7195713177c7c64cfb6acfc66e2f
Rafael Theis <s9rathei@stud.uni-saarland.de>
2014-09-15 16:18:39 +0200

    fixed setters

--- simulator/src/main/java/controller/Translator.java
+++ simulator/src/main/java/controller/Translator.java
@@ -1,21 +1,22 @@
 package controller;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Translator {
 	
 	private List<List<Token>> lex(InputStream textFile){
 		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<List<Token>>();
 	}
 	
 	private List<Command> parse(List<List<Token>> text){
 		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<Command>();
 	}
 	
 	public List<Command> run(InputStream textFile){
-		return null;
+		return new ArrayList<Command>();
 	}
 }
