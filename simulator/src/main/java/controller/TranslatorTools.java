301bd59c31cb2eb3ce599b2ab3e6dc12de847aaa
Andreas <s9anmey2@stud.uni-saarland.de>
2014-09-16 15:08:27 +0200

    translatorneu

--- simulator/src/main/java/controller/TranslatorTools.java
+++ simulator/src/main/java/controller/TranslatorTools.java
@@ -13,27 +13,6 @@ public class TranslatorTools {
 		return comparison;
 	}
 	
-
-	 /*@Specs: isolates the next word
-	 * 
-	 * @Params: the string to check.
-	 * 
-	 * @Returns: the index of the next .*/
-
-	public String nextElement(String line){
-		return line;
-	}
-	
-	/* @Specs: cuts off the string behind the last valid token.
-	 * 
-	 * @Params: the string to cut.
-	 * 
-	 * @Return: the string behind the last valid token.*/
-	
-	public String getAppendix(String line){
-		return line;
-	}
-	
 	/* @Specs: checks, whether the given expression can be converted to an integer, so the translator knows, 
 	 * whether it has to look up a label or convert the given expression to string.
 	 * 
@@ -74,8 +53,8 @@ public class TranslatorTools {
 	
 	/* @Specs: returns the index of the invalid expressions first char within the initial line.*/
 	
-	public int indexOfError(int columns, String line, String currentElement){
-		return columns - getAppendix(line).length() - currentElement.length();
+	public int indexOfError(int columns, String currentLine, String currentElement){
+		return columns - currentLine.length() - currentElement.length();
 
 	}
 	
