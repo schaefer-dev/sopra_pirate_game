3f6213ac584ac875e28b93234a04f8a8e00600ff
Daniel Schäfer <schaefer.dev@gmail.com>
2014-09-16 13:42:03 +0200

    fixed nullpointerexception in FieldTypeTest

--- simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/FieldTypeTest.java
+++ simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/FieldTypeTest.java
@@ -2,6 +2,7 @@ package de.unisaarland.cs.st.pirates.group5.tests;
 
 import static org.junit.Assert.assertTrue;
 import model.Island;
+import model.Map;
 import model.Treasure;
 
 import org.junit.Test;
@@ -10,7 +11,8 @@ public class FieldTypeTest {
 	
 	@Test
 	public void boostCoverageTest(){
-		Island testIsland = new Island(null,0,0,null);
+		Map testMap = new Map(null, null);
+		Island testIsland = new Island(testMap,0,0,null);
 		testIsland.setShip(null);
 		testIsland.moveShip(null);
 		testIsland.placeBuoy(0, null);
