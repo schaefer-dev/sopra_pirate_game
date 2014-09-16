f3cd9536f0e45ddd7f32bbb05f9f1d9f501e868e
Daniel Schäfer <schaefer.dev@gmail.com>
2014-09-16 18:26:24 +0200

    move implementation started but not finished yet

--- simulator/src/main/java/commands/Move.java
+++ simulator/src/main/java/commands/Move.java
@@ -1,5 +1,6 @@
 package commands;
 
+import model.Field;
 import model.Ship;
 import controller.Command;
 
@@ -14,7 +15,35 @@ public class Move implements Command {
 	@Override
 	public void execute(Ship ship) {
 		// TODO Auto-generated method stub
-
+		Field shipField = ship.getPosition();
+		Field targetField = shipField.getNeigbour(ship.relativeToAbsoluteDirection(0));
+		
+		switch(targetField.getFieldType()){
+		
+		case Water:
+			if (targetField.getShip()==null){
+				/* no ship found so ship can move + check for Kraken */
+				if (targetField.getKraken()!=null)
+					ship.changeCondition(-1);
+				shipField.moveShip(targetField);		//TODO careful! moveShip in Field has to check if the ship is still there		
+			}
+			else{
+				/* TODO ship on targetField -> fight + auch kraken nicht vergessen*/
+			}
+			break;
+			
+		case Base:
+			
+			break;
+			
+		case Island: 
+			
+			break;
+			
+		case ProvisionIsland:
+			
+			break;		
+		}
 	}
 	@Override
 	public boolean equals (Object o)
