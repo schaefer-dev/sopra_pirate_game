e89730c0265988c1a75a813da153df68faf431b8
Jan <s9jamenz@stud.uni-saarland.de>
2014-09-17 15:54:44 +0200

    fixed Test

--- simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/ComparisonBoostTest.java
+++ simulator/src/test/java/de/unisaarland/cs/st/pirates/group5/tests/ComparisonBoostTest.java
@@ -1,12 +1,16 @@
 package de.unisaarland.cs.st.pirates.group5.tests;
 
 import static org.junit.Assert.*;
+import model.CellType;
+import model.Register;
+import model.ShipType;
 
 import org.junit.Test;
 
 import controller.BoolComparison;
 import controller.CellTypeComparison;
 import controller.IntComparison;
+import controller.Operator;
 import controller.ShipTypeComparison;
 
 public class ComparisonBoostTest {
@@ -14,14 +18,11 @@ public class ComparisonBoostTest {
 	@Test
 	public void BoostTest(){
 		
-		BoolComparison testBool = new BoolComparison(null,true);
-		CellTypeComparison testCell = new CellTypeComparison(null, null, null);
-		ShipTypeComparison testShip = new ShipTypeComparison(null,null,null);
-		IntComparison testInt = new IntComparison(null,null,null);
-		
-		
-		
-		
+		BoolComparison testBool = new BoolComparison(Register.sense_marker3,true);
+		CellTypeComparison testCell = new CellTypeComparison(Operator.Unequal, Register.sense_celltype, CellType.EnemyHome);
+		ShipTypeComparison testShip = new ShipTypeComparison(Operator.Unequal,Register.sense_shiptype,ShipType.Friend);
+		IntComparison testInt = new IntComparison(Operator.Greater, Register.ship_load, Register.sense_shipcondition);
+	
 		assertTrue(testInt.equals(testInt));
 		assertFalse(testInt.equals(null));
 		assertFalse(testInt.equals(testBool));
