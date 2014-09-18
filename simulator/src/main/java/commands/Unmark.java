876568262f8268d0126b69100a0d4d27de39b940
Janna Herrmann <s9jnherr@stud.uni-saarland.de>
2014-09-18 17:59:01 +0200

    javadoc added

--- simulator/src/main/java/commands/Unmark.java
+++ simulator/src/main/java/commands/Unmark.java
@@ -4,14 +4,30 @@ package commands;
 import model.Ship;
 import controller.Command;
 
+/**
+ * The Unmark class
+ * This class implements Command and describes the action of deleting a buoy of the same team from a field.
+ * @author Janna
+ *
+ */
 public class Unmark implements Command {
 
+	/** the type of the buoy which should me deleted (between 0 and 5)*/
 	private int type;
 	
+	/** Constructor
+	 * Creates a new object of this class.
+	 * @param type	the type of the guoy which should be deleted (between 0 and 5).
+	 */
 	public Unmark(int type){
 		this.type = type;
 	}
 	
+	/**
+	 * Executes the command by calling the method "deleteBuoy(Team team, Type type)" in the class field.
+	 * The check if the buoy is on this field and can be deleted happens in the method in field.
+	 * @param ship 	The ship which should delete a buoy.
+	 */
 	@Override
 	public void execute(Ship ship) {
 
@@ -19,6 +35,7 @@ public class Unmark implements Command {
 		
 
 	}
+	
 	@Override
 	public boolean equals (Object o)
 	{
